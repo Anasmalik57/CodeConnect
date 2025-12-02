9f7d8c1d0c545a7be0efa59dc1de6330e4575898
Anasmalik57 <itsanas474@gmail.com>
2025-12-02 11:21:03 +0530

    minor changes

--- components/Developer/DevCard.js
+++ components/Developer/DevCard.js
@@ -105,7 +105,7 @@ const DevCard = ({ filteredDevelopers }) => {
                     >
                       {dev.name}
                     </h3>
-                  <div className="absolute top-5 right-5">
+                  <div className="absolute top-8 right-7">
                     {dev?.available == true ? <div className="size-3 p-1 bg-green-500 rounded-full animate-pulse" /> : <div className="size-3 p-2 bg-red-500 rounded-full animate-pulse" /> }
                   </div>
                     {/* Rating */}
@@ -136,7 +136,7 @@ const DevCard = ({ filteredDevelopers }) => {
 
               {/* Skills */}
               <div className="flex flex-wrap gap-2 mb-4">
-                {dev.skills.slice(0,5).map((skill) => (
+                {dev.skills.slice(0, 4).map((skill) => (
                   <span key={skill} className="px-3 py-1 bg-linear-to-tl from-blue-600/40 to-sky-600/40 backdrop-blur-md rounded-full text-xs font-medium text-white border border-blue-500/50 shadow-md">
                     {skill}
                   </span>
