6d71527ebc9456cf696a434146f6bde0db6340e7
Anasmalik57 <itsanas474@gmail.com>
2025-12-04 10:39:36 +0530

    maintailning structure

--- components/Developer/DevCard.js
+++ components/Developer/DevCard.js
@@ -1,14 +1,5 @@
 import React, { useState } from "react";
-import {
-  IndianRupee,
-  Sparkles,
-  BadgeCheck,
-  Calendar,
-  X,
-  Languages,
-  Currency,
-  Activity,
-} from "lucide-react";
+import { X, Calendar, Sparkles, Activity, BadgeCheck, IndianRupee, } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { API_BASE } from "@/lib/api";
@@ -251,13 +242,11 @@ const DevCard = ({ filteredDevelopers }) => {
                 {/* Available */}
                 <div className="flex flex-col items-center p-2 bg-purple-900/30 rounded-xl border border-purple-500/30">
                   <Activity className="size-4 text-purple-400 mb-1" />
-                  <span className="text-xs text-purple-300 pb-1">Available</span>
+                  <span className="text-xs text-purple-300 pb-1">
+                    Available
+                  </span>
                   <span className="text-sm font-bold text-white">
-                   {dev?.available == true ? (
-                        "Yes"
-                      ) : (
-                        "No"
-                      )}
+                    {dev?.available == true ? "Yes" : "No"}
                   </span>
                 </div>
               </div>
