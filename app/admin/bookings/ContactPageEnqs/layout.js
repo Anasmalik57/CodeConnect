717f617eff58a1fd63b84b695d41786729c790f3
Anasmalik57 <itsanas474@gmail.com>
2025-12-04 17:28:48 +0530

    completed

--- app/admin/bookings/ContactPageEnqs/layout.js
+++ app/admin/bookings/ContactPageEnqs/layout.js
@@ -9,7 +9,7 @@ export default function ContactPageEnqsLayout({ children }) {
           <div className="relative">
             <div className="absolute inset-0 bg-linear-to-br from-purple-400 via-pink-400 to-red-400  rounded-2xl blur-xl opacity-70" />
             <div className="relative bg-linear-to-br  from-purple-600/20 to-pink-600/20 border border-purple-500/30 p-2 shadow-2xl rounded-xl">
-              <FileText className="size-5 text-cyan-400" strokeWidth={3} />
+              <FileText className="size-5 text-red-400" strokeWidth={3} />
             </div>
           </div>
           <h1 className="text-xl md:text-[25px] font-bold bg-linear-to-r from-purple-400 via-pink-400 to-red-400  bg-clip-text text-transparent ">
